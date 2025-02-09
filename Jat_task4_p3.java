7e20debb68715708a87d760e0a76472fe43579e8
vidyacheni <69033426+vidyacheni@users.noreply.github.com>
2025-02-09 10:39:17 +0530

    Add files via upload

---
+++ Jat_task4_p3.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+public class Jat_task4_p3 {
+
+	public static void main(String[] args) {
+		// write a program to print characters from A to Z
+		for(char ch='A';ch <'Z';ch++)
+		{
+			System.out.println("printing alphabet::" +ch);
+		}
+
+	}
+
+}
