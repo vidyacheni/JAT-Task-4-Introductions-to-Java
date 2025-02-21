d7d2e895c0ccb5e7bbc8593808b9517b84785b77
vidyacheni <69033426+vidyacheni@users.noreply.github.com>
2025-02-21 18:00:50 +0530

    Add files via upload

---
+++ Jat_task4_p8.java
@@ -0,0 +1,15 @@
+package myPackage;
+
+public class Jat_task4_p8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Write a program to Print "Welcome to Guvi" 10 times
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("Welcome to Guvi");
+		}
+
+	}
+
+}
